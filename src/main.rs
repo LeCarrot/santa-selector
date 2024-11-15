21ae19d9fb8067aae84eca1b8c430d4eb88d9b9d
Aric Smith <smith.aric1703@gmail.com>
2024-11-14 21:58:20 -0500

    Start Moving the Shuffle to a Trait Implementation
    I made a trait that I can impliment for Vec that lets me define the
    "santa_shuffle" and use it similarly to how I use "shuffle". This seems
    much closer to the correct solution for what I'm trying to do. I think I
    need to work on making things a little more generic though.
    
    Also, the recursion loop has a fatal flaw. Because we're using "self"
    for the match comparison, the second round is matching against the
    previously shuffled vector, not the original one. I need to capture the
    original vector and somehow make sure I'm always matching against it.

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,57 @@ use rand::seq::SliceRandom;
 use log::{info, debug};
 use std::fs;
 use std::io::ErrorKind;
+// use std::Vec;
 
 #[derive(Parser)]
 struct Args {
     gifters: String,
 }
 
+trait SantaShuffle<T> {
+    fn santa_shuffle(&self) -> Vec<T>;
+}
+
+impl<T: std::cmp::PartialEq + std::clone::Clone> SantaShuffle<T> for Vec<T> {
+    fn santa_shuffle(&self) -> Vec<T> {
+        let mut new_vec = self.clone();
+
+        new_vec.shuffle(&mut thread_rng());
+
+        // the rest of this stuff really breaks down if the vector only has 0
+        // or 1 elements in it. In that case, let's just make this a regular
+        // shuffle
+        if new_vec.len() <= 1 {
+            return new_vec;
+        }
+
+        let matching_elements = self.iter()
+            .zip(new_vec.iter())
+            .filter(|&(a, b)| a == b)
+            .count();
+
+        if matching_elements > 0 {
+            new_vec = new_vec.santa_shuffle();
+        }
+
+        return new_vec.to_vec();
+    }
+}
+
 fn main() {
     env_logger::init();
 
     let args = Args::parse();
-    let gifters: Vec<&str> = args.gifters
-        .split(",")
-        .map(|x| x.trim())
-        .collect();
+    let gifters: Vec<&str> = args.gifters.split(",").map(|x| x.trim()).collect();
 
-    let mut giftees: Vec<&str> = gifters.clone();
+    let giftees = gifters.santa_shuffle();
 
-    loop {
-        giftees.shuffle(&mut thread_rng());
-        let matching = gifters
-            .iter()
-            .zip(giftees.iter())
-            .filter(|&(a, b)| a == b)
-            .count();
-
-        if matching == 0 || giftees.len() <= 1 {
-            break;
-        }
-    }
+    let pairs: Vec<_> = gifters.iter().zip(giftees.iter()).collect();
 
     debug!("Gifters {:?}", gifters);
     debug!("Giftees {:?}", giftees);
-
+    debug!("Pairs {:?}", pairs);
+/*
     // make results directory
     match fs::create_dir("results") {
         Ok(_) => (),
@@ -52,4 +70,5 @@ fn main() {
         let filename = format!("results/{gifter}.txt");
         fs::write(filename, giftees[i]).expect("Unable to write file");
     }
+*/
 }
